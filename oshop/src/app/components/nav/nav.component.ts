2e7c3b6194b1cb2049d30b2ba027dfeb4a440818
Muhammad Waqar <m_waqar20@yahoo.com>
2018-08-08 13:53:22 +0500

    google login signup complete with user name display and also logout

--- oshop/src/app/components/nav/nav.component.ts
+++ oshop/src/app/components/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { Observable } from '../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -8,9 +9,9 @@ import * as firebase from 'firebase';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent{
-user:firebase.User;
+user$:Observable<firebase.User>;
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user=>this.user=user);
+    this.user$=afAuth.authState;
    }
 
   logout(){
